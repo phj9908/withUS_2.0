33be5bcac317191b167c51dbb503b840da678a89
정윤수 <https://github.com/whatpill1/whatpill.git>
2022-12-01 11:32:05 +0900

    sidebar + group + sub

---
+++ src/main/resources/static/js/mainSub.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('.accordion .accordion_item > button').forEach((btn) => {
+//     btn.addEventListener('click', (e) => {
+//         let accordion_item = e.currentTarget.parentNode;
+//         accordion_item.classList.toggle('open');
+//     });
+// });
+
+// var testBtn = document.querySelector('.test-btn');
+//
+// testBtn.addEventListener('click', () =>{
+//     Class.forName("org.mariadb.jdbc.Driver");
+//     Connection conn = DriverManager.getConnection()
+// })
